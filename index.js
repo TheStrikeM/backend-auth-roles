a32557b77845947d0885dad0978984a51bcf1d68
Danil <dinkodanil@gmail.com>
2021-02-27 00:05:00 +0300

    add start function

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express")
 const config = require("config")
+const mongoose = require("mongoose")
 
 
 const PORT = config.get("serverPort") || 5000
 const app = express()
 
 app.use(express.json())
+
+const start = async () => {
+
+    await mongoose.connect(config.get("dbUrl"))
+    app.listen(PORT, () => {
+        console.log(`* Сервер успешно запущен на порту ${PORT}`)
+        console.log(`* Совершенно успешное подключение к MongoDB`)
+        console.log(`* URL DB: ${config.get("dbUrl")}`)
+        console.log(`* Доступно по 127.0.0.1:${PORT}`)
+    })
+}
